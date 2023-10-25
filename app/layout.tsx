debd1c1e230516a842a40c9268fa8ef02c4c07b0
Pavel1841 <birtofhermes1841@gmail.com>
2023-10-25 13:40:05 +0300

    added chat features

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import { cn } from '@/lib/utils'
+
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+
 import { ThemeProvider } from '@/components/providers/theme-provider'
-import { cn } from '@/lib/utils'
 import { ModalProvider } from '@/components/providers/modal-provider'
 import { SocketProvider } from '@/components/providers/socket-provider'
+import { QueryProvider } from '@/components/providers/query-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -31,7 +34,7 @@ export default function RootLayout({
 					>
 						<SocketProvider>
 							<ModalProvider />
-							{children}
+							<QueryProvider>{children}</QueryProvider>
 						</SocketProvider>
 					</ThemeProvider>
 				</body>
